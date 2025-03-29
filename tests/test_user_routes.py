b92ab863085da40b248d4936c76697c345099f40
drizzle021 <gabrielabraham004@gmail.com>
2025-03-29 15:13:23 +0100

    modified user route tests

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -37,5 +37,35 @@ def test_get_users(client, sample_user):
 
     users = response.json
     assert len(users) == 1
-    assert users[0]["username"] == "testuser"
-    assert users[0]["email"] == "testuser@example.com"
+    assert users[0]["username"] == sample_user["username"]
+    assert users[0]["email"] == sample_user["email"]
+
+
+def test_get_users(client, sample_user):
+    """
+    Test the GET /users/<int:id> endpoint to retrieve a specific user
+    """
+    # Add a user to the database
+    user = User(
+        username=sample_user["username"],
+        email=sample_user["email"],
+        password=sample_user["password"],
+        role=Role.ARTIST,
+        profile_picture=sample_user["profile_picture"],
+        banner=sample_user["banner"]
+    )
+    db.session.add(user)
+    db.session.commit()
+
+    response = client.get("/users/1")
+    assert response.status_code == 200
+
+    print("Retrieved User:", response.json)
+
+    user = response.json
+
+    assert user["username"] == sample_user["username"]
+    assert user["email"] == sample_user["email"]
+    assert user["profile_picture"] == sample_user["profile_picture"]
+    assert user["banner"] == sample_user["banner"]
+    
