0ab46b6dde739f93135ddb9f62e90d501459e90e
Akos Levardy <xlevardya@stuba.sk>
2025-04-06 22:48:11 +0200

    edit post, like post, save/unsave post, comment on post, get comments for post - done

--- app/routes/post_routes.py
+++ app/routes/post_routes.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request, jsonify
 from app.services.post_service import PostService
+from app.services.report_service import ReportService
 from app.models.post import PostType
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 post_bp = Blueprint("posts", __name__)
 
 @post_bp.route("/posts", methods=["GET"])
+@jwt_required()
 def get_posts():
     """
     Route to retrieve all posts.
@@ -16,7 +19,9 @@ def get_posts():
 
 
 # divide into novel and illustration maybe
+
 @post_bp.route("/posts", methods=["POST"])
+@jwt_required()
 def add_post():
     """
     Route to create a new post
@@ -30,6 +35,9 @@ def add_post():
     is_spoilered = request.form.get("is_spoilered", "false").lower() == "true"
     software = request.form.get("software")
 
+    identity = get_jwt_identity()
+    print(f"IDENTITY {identity}")
+
     # PICTURES in array? LAZY LOAD
     # picture = request.form.get()
 
@@ -37,12 +45,13 @@ def add_post():
     if not title or not post_type or not caption:
         return jsonify({"error": "Missing required fields"}), 400
 
-    response, status_code = PostService.add_post(title, post_type, caption, description, is_spoilered, software)
+    response, status_code = PostService.add_post(title, post_type, caption, description, is_spoilered, software, identity)
 
     return jsonify(response), status_code
 
 
 @post_bp.route("/posts/<int:id>", methods=["GET"])
+@jwt_required()
 def get_post(id):
     """
     Route to retrieve a post with a specific ID.
@@ -55,55 +64,117 @@ def get_post(id):
 
 
 @post_bp.route("/posts/<int:id>", methods=["DELETE"])
+@jwt_required()
 def delete_post(id):
     """
     Route to delete a post with a specific ID.
     """
-    pass
+    identity = get_jwt_identity()
+    response, status_code = PostService.delete_post(id, identity)
+
+    return jsonify(response), status_code
+
 
 @post_bp.route("/posts/<int:id>/report", methods=["POST"])
+@jwt_required()
 def report_post(id):
     """
     Route to report a post with a specific ID.
     """
-    pass
+
+    report_type = request.form.get("report_type")
+
+    description = request.form.get("description")
+
+    identity = get_jwt_identity()
+
+    # check required fields
+    if not description or not report_type:
+        return jsonify({"error": "Missing required fields"}), 400
+
+    response, status_code = ReportService.add_report(id, report_type, description, identity)
+
+    return jsonify(response), status_code
+
 
 @post_bp.route("/posts/<int:id>/edit", methods=["PUT"])
+@jwt_required()
 def edit_post(id):
     """
     Route to edit a post with a specific ID.
     """
-    pass
+    identity = get_jwt_identity()
+    data = request.form
+
+    response, status_code = PostService.edit_post(id, identity, data)
+
+    return jsonify(response), status_code
+
+
 
 # NO IDEA
 @post_bp.route("/posts/<int:id>/like", methods=["POST"])
+@jwt_required()
 def like_post(id):
     """
     Route to add a like to a post with a specific ID.
     """
-    pass
+    identity = get_jwt_identity()
+    response, status_code = PostService.toggle_like(id, identity)
+    return jsonify(response), status_code
+
+
 
 @post_bp.route("/posts/<int:id>/save", methods=["POST"])
+@jwt_required()
 def save_post(id):
     """
     Route to save a post with a specific ID to saved posts.
     """
-    pass
+    identity = get_jwt_identity()
+    response, status = PostService.save_post(id, identity)
+    return jsonify(response), status
+
+@post_bp.route("/posts/<int:id>/save", methods=["DELETE"])
+@jwt_required()
+def unsave_post(id):
+    identity = get_jwt_identity()
+    response, status = PostService.unsave_post(id, identity)
+    return jsonify(response), status
+
+
+
 
 
 @post_bp.route("/posts/<int:id>/comments", methods=["GET"])
+@jwt_required()
 def get_comments(id):
     """
     Route to retrieve all comments a post with a specific ID.
     """
-    pass
+    response, status = PostService.get_comments(id)
+    return jsonify(response), status
 
 @post_bp.route("/posts/<int:id>/comments", methods=["POST"])
+@jwt_required()
 def add_comment(id):
     """
     Route to add a comment to a post with a specific ID.
     """
+    identity = get_jwt_identity()
+    content = request.form.get("content")
+
+    if not content or content.strip() == "":
+        return jsonify({"error": "Comment content is required"}), 400
+    
+    response, status_code = PostService.add_comment(id, identity, content.strip())
+    return jsonify(response), status_code
+
+
+@post_bp.route("/posts/search", methods=["GET"])
+@jwt_required()
+def search_post():
+    """
+    Route to search for posts with filter.
+    """
     pass
-
-
-
--- app/services/post_service.py
+++ app/services/post_service.py
@@ -1,5 +1,6 @@
-from app.models import Post
+from app.models import Post, User, Like, Comment
 from app.extensions import db
+from app.models.user import saved_posts
 
 class PostService:
     @staticmethod
@@ -11,10 +12,17 @@ class PostService:
         posts = Post.query.all()
         return [
             {
-                "id": post.id,
+            "id": post.id,
+            "title": post.title,
+            "post_type": post.post_type.name,
+            "caption": post.caption,
+            "description": post.description,
+            "is_spoilered": post.is_spoilered,
+            "software": post.software,
+            "author": post.author_id
             }
             for post in posts
-        ]
+        ], 200
     
     @staticmethod
     def get_post(id):
@@ -29,14 +37,23 @@ class PostService:
         if not post:
             return {"error": "Post not found"}, 404
         
+
+
         return {
             "id": post.id,
+            "title": post.title,
+            "post_type": post.post_type.name,
+            "caption": post.caption,
+            "description": post.description,
+            "is_spoilered": post.is_spoilered,
+            "software": post.software,
+            "author": post.author_id
 
         }, 200
 
 
     @staticmethod
-    def add_post(title, post_type, caption, description, is_spoilered, software):
+    def add_post(title, post_type, caption, description, is_spoilered, software, identity):
         """
         Creates a post in the database.
 
@@ -49,6 +66,8 @@ class PostService:
             software
         """
 
+
+
         # TODO: ADD PICTURES
         post = Post(
             title=title,
@@ -56,10 +75,174 @@ class PostService:
             caption=caption,
             description=description,
             is_spoilered=is_spoilered,
-            software=software
+            software=software,
+            author_id=identity
         )
 
         db.session.add(post)
         db.session.commit()
 
         return {"message": "New post created successfully"}, 201
+
+
+    @staticmethod
+    def delete_post(id, identity):
+        """
+        Deletes a post in the database.
+
+        """
+
+        post = db.session.get(Post, id)
+        if not post:
+            return {"error": "Post not found"}, 404
+        
+        
+        logged_in_user = db.session.get(User, identity)
+
+        if logged_in_user.id == post.author_id or logged_in_user.role.name == "MODERATOR":
+            db.session.delete(post)
+            db.session.commit()
+            return {"message": "Post deleted successfully"}, 201
+
+        else:
+            return {"error": "Unauthorized"}, 401
+
+    @staticmethod
+    def edit_post(id, identity, data):
+        """
+        Edits a post in the database.
+
+        """
+
+        post = db.session.get(Post, id)
+        if not post:
+            return {"error": "Post not found"}, 404
+        
+        
+        logged_in_user = db.session.get(User, identity)
+
+
+        if logged_in_user.id != post.author_id:
+            return {"error": "Unauthorized"}, 401
+
+        # Only update if value is provided and not empty
+        if "title" in data and data["title"].strip() != "":
+            post.title = data["title"].strip()
+
+        if "caption" in data and data["caption"].strip() != "":
+            post.caption = data["caption"].strip()
+
+        if "description" in data and data["description"].strip() != "":
+            post.description = data["description"].strip()
+
+        if "software" in data and data["software"].strip() != "":
+            post.software = data["software"].strip()
+
+        # Handle boolean
+        if "is_spoilered" in data:
+            raw = data["is_spoilered"]
+            if isinstance(raw, bool):
+                post.is_spoilered = raw
+            elif isinstance(raw, str):
+                post.is_spoilered = raw.lower() == "true"
+
+        db.session.commit()
+        return {"message": "Post updated successfully"}, 200
+    
+    
+    @staticmethod
+    def toggle_like(post_id, user_id):
+        """
+        Toggles like for a post: adds like if not liked, removes like if already liked.
+        """
+        post = db.session.get(Post, post_id)
+        if not post:
+            return {"error": "Post not found"}, 404
+
+        existing_like = Like.query.filter_by(post_id=post_id, user_id=user_id).first()
+
+        if existing_like:
+            db.session.delete(existing_like)
+            db.session.commit()
+            return {"message": "Post unliked"}, 200
+
+        new_like = Like(post_id=post_id, user_id=user_id)
+        db.session.add(new_like)
+        db.session.commit()
+        return {"message": "Post liked"}, 201
+
+    @staticmethod
+    def add_comment(post_id, user_id, content):
+        """
+        Creates comment for a Post in database
+        """
+        post = db.session.get(Post, post_id)
+        if not post:
+            return {"error": "Post not found"}, 404
+
+        comment = Comment(post_id=post_id, author_id=user_id, content=content)
+
+        db.session.add(comment)
+        db.session.commit()
+
+        return {"message": "Comment added successfully"}, 201
+    
+    @staticmethod
+    # pagination here maybe
+    def get_comments(id):
+        """
+        Retrieves all comments for a Post from the database.
+        """
+        post = db.session.get(Post, id)
+        if not post:
+            return {"error": "Post not found"}, 404
+        
+        comments = Comment.query.filter_by(post_id=id).order_by(Comment.created_at.asc()).all()
+
+        return [
+            {
+                "id": comment.id,
+                "content": comment.content,
+                "author": comment.author_id,
+                "post_id": comment.post_id,
+                "created_at": comment.created_at
+            }
+            for comment in comments
+        ], 200
+
+    @staticmethod
+    def save_post(post_id, user_id):
+        post = db.session.get(Post, post_id)
+        if not post:
+            return {"error": "Post not found"}, 404
+
+        exists = db.session.execute(
+            db.select(saved_posts).where(
+                saved_posts.c.user_id == user_id,
+                saved_posts.c.post_id == post_id
+            )
+        ).first()
+
+        if exists:
+            return {"error": "Post already saved"}, 409
+
+        db.session.execute(
+            saved_posts.insert().values(user_id=user_id, post_id=post_id)
+        )
+        db.session.commit()
+        return {"message": "Post saved successfully"}, 201
+
+    @staticmethod
+    def unsave_post(post_id, user_id):
+        result = db.session.execute(
+            saved_posts.delete().where(
+                saved_posts.c.user_id == user_id,
+                saved_posts.c.post_id == post_id
+            )
+        )
+        db.session.commit()
+
+        if result.rowcount == 0:
+            return {"error": "Post not saved"}, 404
+
+        return {"message": "Post unsaved successfully"}, 200
